2a00a9159e25f442c45f8aa0f55c34aa5faff6cc
Vadim <dim779564@gmail.com>
2017-08-02 02:37:34 +0300

    'fullmock'

---
+++ src/app/landlord/landlord.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+
+
+
+@Component({
+  selector: 'landlord-root',
+  templateUrl: 'landlord.component.html',    
+  styleUrls: ['landlord.component.css']
+
+})
+
+export class Landlord{}
